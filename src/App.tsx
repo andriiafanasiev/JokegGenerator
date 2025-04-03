877a0ea815f9b714eb904d98c25688211145a4b6
andriiafanasiev <andreynoobroo@gmail.com>
2025-04-03 10:24:46 +0200

    Added basic functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,39 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+    const [joke, setJoke] = useState<string>('');
+
+    async function getTheJoke(): Promise<void> {
+        try {
+            const response = await fetch('https://v2.jokeapi.dev/joke/Any');
+            const data = await response.json();
+            if (!data.joke) {
+                setJoke(`- ${data.setup}\n - ${data.delivery}`);
+            } else {
+                setJoke(data.joke);
+            }
+
+            console.log(data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
     return (
         <>
             <div>
                 <h1>Joke generator</h1>
-                <p>Just click to "Get a Joke" button</p>
-                <button>Get a Joke</button>
+                <p className="mt-2">Just click to "Get a Joke" button</p>
+
+                <pre className="mt-5 mb-10">{joke}</pre>
+                <button
+                    onClick={() => {
+                        getTheJoke();
+                    }}
+                >
+                    Get a Joke
+                </button>
             </div>
         </>
     );
