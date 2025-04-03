ef918ae42dbdae6142c5d6ce26dbcfc4e7dd3f73
andriiafanasiev <andreynoobroo@gmail.com>
2025-04-03 09:43:33 +0200

    all files via uploaded

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+    return (
+        <>
+            <div>
+                <h1>Joke generator</h1>
+                <p>Just click to "Get a Joke" button</p>
+                <button>Get a Joke</button>
+            </div>
+        </>
+    );
+}
+
+export default App;
